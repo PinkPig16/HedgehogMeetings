29f45fa532772f9ce4a8cb62d834bc634987d690
ArtemDaskalica <808nosky808@gmail.com>
2024-10-16 16:46:34 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using BenchmarkDotNet.Running;
+using hedgehog;
+
+int neededHedgehog = 2;
+                       
+var hedgehogPopulation = new List<int> { 456, 153, 4 };
+
+var ServiceHedgehog = new ServiceHedgehog();
+
+var countOperation = ServiceHedgehog.MinimumMeetings(neededHedgehog, hedgehogPopulation);
+
+Console.WriteLine(countOperation);
---
+++ ServiceHedgehog.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hedgehog
+{
+    public class ServiceHedgehog
+    {
+        public int MinimumMeetings(int neededHedgehog, List<int> hedgehogPopulation)
+
+        {
+            //Получаем индексты двух других ёжей
+            int firstHedgehog = (neededHedgehog + 1) % 3;
+            int secondHedgehog = (neededHedgehog + 2) % 3;
+
+            if (hedgehogPopulation[firstHedgehog] == 0 && hedgehogPopulation[secondHedgehog] == 0)
+            {
+                return 0;
+            }else if(hedgehogPopulation[firstHedgehog] == hedgehogPopulation.Sum() || hedgehogPopulation[secondHedgehog] == hedgehogPopulation.Sum())
+            {
+                return -1;
+            }
+
+            //Если условие не будет выполнятся то невозможно будет привести двух других ёжей к одинаковому кол-ву
+            //Что необходимо для приведения к одному цвету
+            if ((hedgehogPopulation[firstHedgehog] - hedgehogPopulation[secondHedgehog]) % 3 == 0) 
+            {
+                int countOperation = 0;
+                //Распределение элементов к среднему 
+                while (Math.Abs(hedgehogPopulation[firstHedgehog] - hedgehogPopulation[secondHedgehog]) != 3)
+                {
+
+                    int minIndex = 2;
+
+                    if (hedgehogPopulation[0] <= hedgehogPopulation[1] && hedgehogPopulation[0] <= hedgehogPopulation[2])
+                    {
+                        minIndex = 0;
+                    }
+                    else if (hedgehogPopulation[1] <= hedgehogPopulation[0] && hedgehogPopulation[1] <= hedgehogPopulation[2])
+                    {
+                        minIndex = 1;
+                    }
+
+                    Console.WriteLine($"{hedgehogPopulation[0]} {hedgehogPopulation[1]} {hedgehogPopulation[2]} \n");
+
+                    //Встреча ёжиков
+                    hedgehogPopulation[(minIndex + 1) % 3]--;
+                    hedgehogPopulation[(minIndex + 2) % 3]--;
+                    hedgehogPopulation[minIndex] += 2;
+
+                    countOperation++;
+                }
+
+                var min = hedgehogPopulation[firstHedgehog] < hedgehogPopulation[secondHedgehog] ? firstHedgehog : secondHedgehog;
+
+                Console.WriteLine($"{hedgehogPopulation[0]} {hedgehogPopulation[1]} {hedgehogPopulation[2]} \n");
+                //Встреча ёжиков
+                hedgehogPopulation[(min + 1) % 3]--;
+                hedgehogPopulation[(min + 2) % 3]--;
+                hedgehogPopulation[min] += 2;
+
+                countOperation++;
+
+                while (hedgehogPopulation[firstHedgehog] + hedgehogPopulation[secondHedgehog] != 0)
+                {
+                    Console.WriteLine($"{hedgehogPopulation[0]} {hedgehogPopulation[1]} {hedgehogPopulation[2]} \n");
+
+                    //Встреча ёжиков
+                    hedgehogPopulation[firstHedgehog]--;
+                    hedgehogPopulation[secondHedgehog]--;
+                    hedgehogPopulation[neededHedgehog]+=2;
+
+                    countOperation++;
+                }
+                return countOperation;
+            }
+
+            return -1;
+        }
+
+    }
+}
